851d0174d8e984551e2a0764bc9d23bb6d917d4e
ilovethedrama <bluemagicuk@yahoo.co.uk>
2023-07-04 23:03:12 +0100

    fix: update prod uri

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,10 +1,17 @@
 import { MongoClient } from 'mongodb';
 
-if (!process.env.MONGODB_URI) {
-  throw new Error("Fam that 'MONGODB_URI' is mythical! It doesn't exist");
+const uri = `mongodb+srv://${process.env.CLUSTER_NAME}_${process.env.DB_PRIMARY_USER}:${process.env.MONGODB_PASSWORD}@kyukdb.4q2mr66.mongodb.net/?retryWrites=true&w=majority`
+
+if (!process.env.CLUSTER_NAME) {
+  throw new Error("Fam that 'URI' is mythical, it's malformed due to the CLUSTER_NAME! It doesn't exist");
+}
+if (!process.env.DB_PRIMARY_USER) {
+  throw new Error("Fam that 'URI' is mythical, it's malformed due to the DB_PRIMARY_USER! It doesn't exist");
+}
+if (!process.env.MONGODB_PASSWORD) {
+  throw new Error("Fam that 'URI' is mythical, it's malformed due to the MONGODB_PASSWORD! It doesn't exist");
 }
 
-const uri = process.env.MONGODB_URI;
 const options = {};
 
 let client;
