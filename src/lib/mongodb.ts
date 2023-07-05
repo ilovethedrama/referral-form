93a1d9350d8b092c9394c0ab297b5237e88ea292
ilovethedrama <bluemagicuk@yahoo.co.uk>
2023-07-05 20:35:52 +0100

    fix: trigger redeploy

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -10,7 +10,7 @@ if (!process.env.DB_USER) {
 }
 
 if (process.env.DB_CLUSTER) {
-  throw new Error("Fam that 'URI' is mythical, it's malformed due to the DB_CLUSTER! It (does)n't exist");
+  throw new Error(`Fam that 'URI' is mythical, it's malformed due to the DB_CLUSTER: ${process.env.DB_CLUSTER}! It (does)n't exist`);
 }
 
 
