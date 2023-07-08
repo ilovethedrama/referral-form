c2e90a45d7ad5e91c341ca1c4afa4e5d2100f408
ilovethedrama <bluemagicuk@yahoo.co.uk>
2023-07-08 01:14:27 +0100

    Fix: Use env vars and add get route to return organisation profile

---
+++ src/app/organisations/profile/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { fetchProfile } from "@/components/submitHandler";
+import { useQuery } from "@tanstack/react-query";
+
+export default function Home() {
+  const { isLoading, isError, data, error } = useQuery({
+    queryKey: ["profile"],
+    queryFn: fetchProfile,
+  });
+
+  if (isLoading) {
+    return <span>Loading son!...</span>;
+  }
+
+  if (isError) {
+    return <span>Hmmm there&apos;s been an error son!...</span>;
+  }
+
+  return (
+    <main>
+      <div>
+        <h1>Welcome to profile home!</h1>
+        <main>
+          <h3>{data?.body?.firstName || 'No record found'}</h3>
+        </main>
+      </div>
+    </main>
+  );
+}
--- src/components/submitHandler.tsx
+++ src/components/submitHandler.tsx
@@ -1,32 +1,49 @@
-'use server'
+"use server";
 
 type IReferralFormInput = {
-    firstName: string;
-    lastName: string;
-    title: string;
-    gender: string;
-    address: string;
-    contactNumber: string;
-    dateOfBirth: string;
-    email: string;
-    reasonForNEET: string;
-    referrerFullName: string;
-    referrerRelationshipType: string;
-    reasonForReferral: string;
-  };
-  
-   export async function handleLeSubmit (data: IReferralFormInput) {
-    try {
-      let response = await fetch(`${process.env.APP_HOST}/api/testing`, {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json",
-        },
-      });
-      await response.json();
-    } catch (errorMessage: any) {
-      console.log(errorMessage);
-    }
-  };
+  firstName: string;
+  lastName: string;
+  title: string;
+  gender: string;
+  address: string;
+  contactNumber: string;
+  dateOfBirth: string;
+  email: string;
+  reasonForNEET: string;
+  referrerFullName: string;
+  referrerRelationshipType: string;
+  reasonForReferral: string;
+};
+
+export async function postReferralForm(data: IReferralFormInput) {
+  try {
+    let response = await fetch(`${process.env.APP_HOST}/api/testing`, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+    });
+    await response.json();
+  } catch (errorMessage: any) {
+    console.log(errorMessage);
+  }
+}
+
+export async function fetchProfile() {
+  try {
+    let response = await fetch(`${process.env.APP_HOST}/api/testing`, {
+      next: { revalidate: 0},
+      method: "GET",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await response.json();
+    return result;
+  } catch (errorMessage: any) {
+    console.log(errorMessage);
+  }
+}
